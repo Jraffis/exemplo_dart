313f798e427c5f4a7c94575c15e2bf38981dae2a
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-02 08:42:35 -0300

    Novo

---
+++ aula01.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Variável de tipo dinâmico
+  var nome = 'Dieimes';
+
+  // Variáveis com tipos específicos
+  int idade = 16;
+  double altura = 1.65;
+  String cidade = 'Mônaco';
+  bool possuiCarro = true;
+
+  print(nome);
+  print(idade);
+  print(altura);
+  print(cidade);
+  print(possuiCarro);
+}
