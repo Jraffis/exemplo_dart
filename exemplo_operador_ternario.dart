e1f00521912165c5a2e1c3be318815c5edbe4ae7
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-02 10:25:16 -0300

    Atualização

---
+++ exemplo_operador_ternario.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Operador Ternário
+  int idade = 18;
+  String msg = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+  print(msg);
+}
---
+++ exemplo_switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  String linguagem = "html";
+
+  switch(linguagem){
+    case "html":
+      print("Linguagem de marcação legal");
+        break;
+    case "php":
+      print("Linguagem de programação super legal");
+        break;
+    case "javascript":
+    print("Interessante");
+      break;
+    default:
+      print("Não conheço");
+        break;
+  }
+  
+}
