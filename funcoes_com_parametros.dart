bbeec0abd3cf0a3f6f935a814a9c3e10ec69d272
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-03 10:05:29 -0300

    Nova Atualização

---
+++ funcoes_com_parametros.dart
@@ -0,0 +1,11 @@
+void main() {
+  jedi("Dieimes", "Blake");
+  jedi("Dieimes", "Brandon");
+  jedi("Dieimes", "Chase");
+  jedi("Dieimes", "Steven");
+}
+
+void jedi(String professor, aluno) {
+  print("O professor $professor é um Mestre Jedi");
+  print("Os aluno $aluno é Padawan");
+}
