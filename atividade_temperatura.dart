f036d4301eaf6e9e224622ef1408167d2676d65b
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-03 11:05:02 -0300

    Nova Atualização

--- atividade_temperatura.dart
+++ atividade_temperatura.dart
@@ -1,6 +1,6 @@
 void main() {
   double c = 20;
-  double f = (9* c + 160)/5;
+  double f = (9 * c + 160)/5;
   print("A temperatura em Celsius é: $c");
   print("A temperatura em Fahrenheit é: $f");
 }
