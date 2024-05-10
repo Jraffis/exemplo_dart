2b094302fea8796fa9aae2c81458f50ddb4b6c96
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-10 11:29:17 -0300

    Nova Atualização

--- programacao_orientada_a_objeto/exemplo01.dart
+++ programacao_orientada_a_objeto/exemplo01.dart
@@ -1,10 +1,19 @@
+class Pessoa {
+  String? nome; // Atributos
+  int? idade; // Atributos
+
+  void mensagem() {
+    print("Parabéns $nome, seja bem-vindo!");
+  }
+}
+
 void main() {
   Pessoa pessoa1 = Pessoa();
   pessoa1.nome = "Dieimes";
   pessoa1.idade = 33;
-}
 
-class Pessoa {
-  String? nome;
-  int? idade;
+  print(pessoa1.nome);
+  print(pessoa1.idade);
+
+  pessoa1.mensagem();
 }
