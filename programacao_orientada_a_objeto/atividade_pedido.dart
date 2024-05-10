9d78ee80151e0cd3cc328ceb57f601fc3fca6a69
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-05-10 12:05:54 -0300

    Nova Atualização

---
+++ programacao_orientada_a_objeto/atividade_pedido.dart
@@ -0,0 +1,28 @@
+void main() {
+  Pedido pedido1 = Pedido(001, "Dieimes", ["Blusa"]);
+
+  pedido1.exibirPedido();
+
+}
+
+class Pedido {
+  int numero;
+  String nome_cliente;
+  List<String> itens = [];
+
+  Pedido(this.numero, this.nome_cliente, this.itens);
+
+  void adicionarItens(String item) {
+    itens.add(item);
+  }
+
+  void removerItens(String item) {
+    itens.remove(item);
+  }
+
+  void exibirPedido() {
+    for(String item in itens) {
+      print(item);
+    }
+  }
+}
